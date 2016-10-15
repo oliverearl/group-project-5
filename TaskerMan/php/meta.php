805a2dfe37104d26c0f686eb5034242b9b4d1fc6
David Fairbrother <DavidFair@users.noreply.github.com>
2016-10-15 16:06:16 +0100

    Tidy folders to how they should have been done

---
+++ TaskerMan/php/meta.php
@@ -0,0 +1,7 @@
+<!-- Metadata -->
+<meta charset = "utf-8" />
+<meta name = "author" content = "<?php echo GROUP . ' ' . COPYRIGHT; ?>" />
+<meta name = "description" content ="<?php echo APP_DESC; ?>" />
+<!-- Includes -->
+<link href="css/style.css" rel="stylesheet" />
+<!-- Scripts -->
